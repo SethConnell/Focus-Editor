06301f00c70c2ff9e90cfbad50daf5b3fffc5211
Seth Connell <sethjordanconnell@gmail.com>
2017-07-07 14:40:05 -0500

    Added Functionality and Style

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,23 @@ $(document).ready(function(){
 			screenfull.request(target);
 		}
 	});
+	$("#save").click(function() {
+		var element = document.createElement('a');
+		var text = $("#writing").val();
+		var filename = "Writing.txt";
+		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	    element.setAttribute('download', filename);
+	    element.style.display = 'none';
+	    document.body.appendChild(element);
+	    element.click();
+	    document.body.removeChild(element);
+		  // Start file download.
+		var name = $("#docname").val();
+			if (name != "") {
+				download(name + ".txt","This is the content of my file :)");	
+			}
+			else {
+				download("FocusedWritingFile.txt","This is the content of my file :)")
+			}
+	});
 });
